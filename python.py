81786132c6d5bb7ad7f46950254329106e8bd043
MOODY Greg <gmoody@altranuk.local>
2019-09-09 14:08:29 +0100

    Finished Fizz Buzz script

--- python.py
+++ python.py
@@ -1,18 +1,29 @@
-for x in range(1,101):
-    result=[]
-    result=x
-    if x % 3 == 0 and x % 5 != 0:
-        result = "Fizz"
-    if x % 5 == 0 and x % 3 != 0:
-        result = "Buzz"
-    if x % 5 == 0 and x % 3 == 0:
-        result = "FizzBuzz"
-    if x % 7 == 0 and not isinstance(result, str):
-        result = "Bang"
-    if x % 7 == 0 and isinstance(result, str):
-        result = result+"Bang"
-    if result == "BangBang":
-        result = "Bang"
+import sys
+for x in range(1, 301):
+    result = []
+    if x % 3 == 0:
+        result.append("Fizz")
+    if x % 5 == 0:
+        result.append("Buzz")
+    if x % 7 == 0:
+        result.append("Bang")
     if x % 11 == 0:
-        result = "Bong"
-    print(x, result)
+        result = ["Bong"]
+    if x % 13 == 0:
+        if not result:
+            result.append("Fezz")
+        else:
+            found = False
+            for index in range(len(result)):
+                if result[index][0] == "B":
+                    result.insert(index, "Fezz")
+                    found = True
+                    break
+            if not found:
+                result.append("Fezz")
+    if x % 17 == 0:
+        result.reverse()
+    if result:
+        print(str(x) + ": " + "".join(result))
+    else:
+        print(x)
